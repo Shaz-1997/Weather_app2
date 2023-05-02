0e1444f0fe0102ac1762cd78b345ec6951e5fc2f
Shaz-1997 <shanzaeahamed97@gmail.com>
2023-05-02 13:00:58 +0530

    message

---
+++ lib/src/location_page.dart
@@ -0,0 +1,71 @@
+/*
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+
+class LocationPage extends StatefulWidget {
+  const LocationPage({Key? key}) : super(key: key);
+
+  @override
+  State<LocationPage> createState() => _LocationPageState();
+}
+
+class _LocationPageState extends State<LocationPage> {
+  //Create a variable that contains location
+  var locationMessage ="";
+  void getCurrentLocation() async{
+    var position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+  var lastPosition = await Geolocator.getLastKnownPosition();
+  print(lastPosition);
+
+  setState(() {
+    locationMessage = "$position.latitude  , $position.longitude";
+  });
+  }
+  //Function to get the current location
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Location page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.location_on,
+              size: 46.0,
+              color: Colors.blue,
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "Get user location",
+              style: TextStyle(
+                  fontSize: 26.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black12),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(locationMessage),
+            ElevatedButton(
+                onPressed: () {
+                  getCurrentLocation();
+                },
+                child: Text(
+                  "Get User Location",
+                  style: TextStyle(color: Colors.white),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}   */
